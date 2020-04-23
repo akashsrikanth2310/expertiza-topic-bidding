c41d43d8da5e49e3ea05b7ea1c76aae2458f11c2
Akash Srikanth <akashsrikanth14@gmail.com>
2020-04-22 20:41:14 -0500

    Update main.py
    adding extra comment to the Main.py file

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from app import app
 
 if __name__ == "__main__":
-    app.debug = True
-    app.run()
+    app.debug = True #setting the debug mode to make error finding easier
+    app.run() #run the entire flask application
