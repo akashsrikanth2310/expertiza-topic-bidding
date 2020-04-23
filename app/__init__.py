7e172dfa000bdaa3d1e224a35db1af293700a8a1
Akash Srikanth <akashsrikanth14@gmail.com>
2020-04-22 20:45:01 -0500

    Update __init__.py
    Adding necessary refactors

--- app/__init__.py
+++ app/__init__.py
@@ -5,13 +5,13 @@ from app import matching_model
 
 app= Flask(__name__,static_folder=None)
 
-@app.route("/match", methods=['POST'])
+@app.route("/match", methods=['POST']) #using the post method with /match in the url to get the required app route
 def matching():
-    if not request.json:
-        abort(400)
-    bidding_data = json_unpacker.JsonUnpacker(request.json)
+    if not request.json: #will abort the request if it fails to load the json
+        abort(400)  #will have a return status of 400 in case of failure
+    bidding_data = json_unpacker.JsonUnpacker(request.json) #calles the json_unpacker to get the necessary bidding_data
     model = matching_model.MatchingModel(bidding_data.student_ids,
                                 bidding_data.topic_ids,
                                 bidding_data.student_preferences_map,
-                                bidding_data.topic_preferences_map, bidding_data.q_S)
-    return jsonify(model.get_matching())
+                                bidding_data.topic_preferences_map, bidding_data.q_S) #model to get the student_ids,topic_ids,student_preference_map,topic_prefernce_map
+    return jsonify(model.get_matching()) #returns a json object
